2487c2a3898af3db727aa79063429d4e8ab52bdf
AlexeiRabushko <ljo6e4kin@gmail.com>
2022-08-19 16:49:11 +0300

    user_converter_refactoring

--- src/main/java/com/example/kindergarten/converter/UserConverter.java
+++ src/main/java/com/example/kindergarten/converter/UserConverter.java
@@ -1,6 +1,8 @@
 package com.example.kindergarten.converter;
 
+import com.example.kindergarten.dto.ChildDto;
 import com.example.kindergarten.dto.UserDto;
+import com.example.kindergarten.entity.ChildEntity;
 import com.example.kindergarten.entity.UserEntity;
 import java.util.Optional;
 
@@ -11,16 +13,14 @@ public class UserConverter {
             return Optional.empty();
         }
 
-        UserEntity userEntity = UserEntity.builder()
-            .username(userDto.getUsername())
-            .password(userDto.getPassword())
-            .role(userDto.getRole())
-            .accountNumber(userDto.getAccountNumber())
-            .address(userDto.getAddress())
+        return Optional.ofNullable(UserEntity.builder()
+                .username(userDto.getUsername())
+                .password(userDto.getPassword())
+                .role(userDto.getRole())
+                .accountNumber(userDto.getAccountNumber())
+                .address(userDto.getAddress())
                 .contactPhone(userDto.getContactPhone())
-            .build();
-
-        return Optional.of(userEntity);
+                .build());
     }
 
     public static Optional<UserDto> toDto(UserEntity userEntity) {
@@ -28,15 +28,14 @@ public class UserConverter {
             return Optional.empty();
         }
 
-        UserDto userDto = UserDto.builder()
+        return Optional.ofNullable(UserDto.builder()
                 .username(userEntity.getUsername())
                 .password(userEntity.getPassword())
                 .role(userEntity.getRole())
                 .accountNumber(userEntity.getAccountNumber())
                 .address(userEntity.getAddress())
                 .contactPhone(userEntity.getContactPhone())
-                .build();
-        return Optional.of(userDto);
+                .build());
     }
 
 }
