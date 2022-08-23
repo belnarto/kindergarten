e6417469ffa4ecda4b52d77571887a93bdd45375
snoof <test@test.tst>
2022-08-23 17:34:41 +0300

    fix of the function of adding, updating and receiving a child

--- src/main/java/com/example/kindergarten/service/ChildServiceImpl.java
+++ src/main/java/com/example/kindergarten/service/ChildServiceImpl.java
@@ -8,10 +8,12 @@ import com.example.kindergarten.entity.UserEntity;
 import com.example.kindergarten.exception.NotAllowedException;
 import com.example.kindergarten.repository.ChildRepository;
 import com.example.kindergarten.repository.UserRepository;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -27,13 +29,12 @@ public class ChildServiceImpl implements ChildService {
 
     @Override
     public long save(ChildDto childDto) {
-        ChildEntity childEntity = new ChildEntity();
-        setCommonFieldsFromDtoToEntity(childDto, childEntity);
+        ChildEntity childEntity = setCommonFieldsFromDtoToEntity(childDto);
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         UserEntity user = userRepository.findByUsername(userDetails.getUsername())
-            .orElseThrow(() -> new RuntimeException("user not found"));
+                .orElseThrow(() -> new RuntimeException("user not found"));
 
         childEntity.setUser(user);
 
@@ -57,6 +58,9 @@ public class ChildServiceImpl implements ChildService {
                 .age(childEntity.getAge())
                 .sex(childEntity.getSex())
                 .contactPhones(childEntity.getContactPhones())
+                .category(childEntity.getCategory())
+                .birthdate(childEntity.getBirthdate())
+                .updatedAt(childEntity.getUpdatedAt())
                 .build();
     }
 
@@ -78,9 +82,15 @@ public class ChildServiceImpl implements ChildService {
     public boolean update(Long id, ChildDto childDto) {
         if (childRepository.existsById(id)) {
             if (isChildOwnedByUser(id, SecurityContextHolder.getContext().getAuthentication())) {
-                ChildEntity childEntity = new ChildEntity();
+
+                Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+                UserEntity user = userRepository.findByUsername(userDetails.getUsername())
+                        .orElseThrow(() -> new RuntimeException("user not found"));
+
+                ChildEntity childEntity = setCommonFieldsFromDtoToEntity(childDto);
+                childEntity.setUser(user);
                 childEntity.setId(id);
-                setCommonFieldsFromDtoToEntity(childDto, childEntity);
                 childRepository.save(childEntity);
                 return true;
             } else {
@@ -93,26 +103,26 @@ public class ChildServiceImpl implements ChildService {
     @Override
     public List<ChildDto> searchByCategory(String category) {
         return childRepository.findByCategoryIgnoreCaseOrderByUpdatedAtDesc(category).stream()
-            .map(this::setCommonFieldsFromEntityToDto)
-            .collect(Collectors.toList());
+                .map(this::setCommonFieldsFromEntityToDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<ChildDto> searchByName(String name) {
         return childRepository.findByPartName(name).stream()
-            .map(this::setCommonFieldsFromEntityToDto)
-            .collect(Collectors.toList());
+                .map(this::setCommonFieldsFromEntityToDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<ChildDto> searchByAge(int minAge, int maxAge) {
         return childRepository.findByAgeGreaterThanAndAgeLessThanEqualOrderByAgeAsc(minAge, maxAge).stream()
-            .map(this::setCommonFieldsFromEntityToDto)
-            .collect(Collectors.toList());
+                .map(this::setCommonFieldsFromEntityToDto)
+                .collect(Collectors.toList());
     }
 
-    private void setCommonFieldsFromDtoToEntity(ChildDto childDto, ChildEntity childEntity) {
-         ChildEntity.builder()
+    private ChildEntity setCommonFieldsFromDtoToEntity(ChildDto childDto) {
+        return ChildEntity.builder()
                 .firstName(childDto.getFirstName())
                 .lastName(childDto.getLastName())
                 .category(childDto.getCategory())
@@ -120,29 +130,29 @@ public class ChildServiceImpl implements ChildService {
                 .birthdate(childDto.getBirthdate())
                 .contactPhones(childDto.getContactPhones())
                 .sex(childDto.getSex())
-                .updatedAt(childDto.getUpdatedAt())
+                .updatedAt(LocalDateTime.now())
                 .build();
     }
 
     private ChildDto setCommonFieldsFromEntityToDto(ChildEntity childEntity) {
         return ChildDto.builder()
-            .firstName(childEntity.getFirstName())
-            .lastName(childEntity.getLastName())
-            .category(childEntity.getCategory())
-            .age(childEntity.getAge())
-            .birthdate(childEntity.getBirthdate())
-            .contactPhones(childEntity.getContactPhones())
-            .sex(childEntity.getSex())
-            .updatedAt(childEntity.getUpdatedAt())
-            .build();
+                .firstName(childEntity.getFirstName())
+                .lastName(childEntity.getLastName())
+                .category(childEntity.getCategory())
+                .age(childEntity.getAge())
+                .birthdate(childEntity.getBirthdate())
+                .contactPhones(childEntity.getContactPhones())
+                .sex(childEntity.getSex())
+                .updatedAt(childEntity.getUpdatedAt())
+                .build();
     }
 
     private boolean isChildOwnedByUser(Long childId, Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         return childRepository.findById(childId)
-            .map(childEntity -> childEntity.getUser() != null &&
-                hasText(childEntity.getUser().getUsername()) &&
-                childEntity.getUser().getUsername().equalsIgnoreCase(userDetails.getUsername()))
-            .orElse(false);
+                .map(childEntity -> childEntity.getUser() != null &&
+                        hasText(childEntity.getUser().getUsername()) &&
+                        childEntity.getUser().getUsername().equalsIgnoreCase(userDetails.getUsername()))
+                .orElse(false);
     }
 }
